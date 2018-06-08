3c82e1c6a27a5d3bb0aaa1141b555c0415fee679
Ramzi Abdulahi <ramzi_abdullahi@outlook.com>
2018-06-08 01:10:32 -0400

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+/*Most vue.js single page compenents follow this structure of
+html within template, core code in scripts, and css in style */
+<template>
+    <div class="pure-menu pure-menu-horizontal">
+        <div class="container">
+        <a href="#" class="pure-menu-heading pure-menu-link">Home</a>
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">News</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Sports</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Finance</a></li>
+        </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+.pure-menu{
+  background-color: #E17842;
+}
+.pure-menu a{
+  color: white;
+}
+</style>
---
+++ src/components/Single.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="container">
+  <h2>{{ story.title }}</h2>
+         <p>Score: {{ story.score }}</p>
+         <p>{{ story.url }}</p>
+         <div v-for="comment in comments" :key="comment">
+           <div class="comment-wrap">
+                 <div class="comment-block">
+                     <p class="comment-text">{{ comment.text }}</p>
+                     <div class="bottom-comment">
+                         <div class="comment-author">{{ comment.by }}</div>
+                         <div class="comment-date">{{ comment.time }}</div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Single',
+  data: function () {
+    return {
+      story: {},
+      comments: [],
+      url: this.$route.params.id // because of vue-route being installed, we can use $route to give us the route paramameters
+    }
+  },
+  created: function () {
+    axios.get('https://hacker-news.firebaseio.com/v0/item/' + this.url + '.json')
+  .then(result => {
+    this.story = result.data
+    this.story.comments = []
+    this.story.kids.forEach(id => {
+      axios.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json')
+      .then(result => {
+        this.$nextTick(() => { // Defer the callback to be executed after the next DOM update cycle
+          console.log(result.data)
+          this.comments.push(result.data)
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    })
+  })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+}
+</script>
+
+<style scoped>
+.comment-wrap {
+  margin-bottom: 1.25rem;
+  display: table;
+  width: 100%;
+  min-height: 5.3125rem;
+}
+
+.photo {
+  padding-top: 0.625rem;
+  display: table-cell;
+  width: 3.5rem;
+}
+.photo .avatar {
+  height: 2.25rem;
+  width: 2.25rem;
+  border-radius: 50%;
+  background-size: contain;
+}
+
+.comment-block {
+  padding: 1rem;
+  background-color: #fff;
+  display: table-cell;
+  vertical-align: top;
+  border-radius: 0.1875rem;
+  -webkit-box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.08);
+          box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.08);
+}
+.comment-block textarea {
+  width: 100%;
+  resize: none;
+}
+
+.comment-text {
+  margin-bottom: 1.25rem;
+}
+
+.bottom-comment {
+  color: #acb4c2;
+  font-size: 0.875rem;
+}
+
+.comment-date {
+  float: left;
+}
+
+.comment-actions {
+  float: right;
+}
+.comment-actions li {
+  display: inline;
+  margin: -2px;
+  cursor: pointer;
+}
+.comment-actions li.complain {
+  padding-right: 0.75rem;
+  border-right: 1px solid #e1e5eb;
+}
+.comment-actions li.reply {
+  padding-left: 0.75rem;
+  padding-right: 0.125rem;
+}
+.comment-actions li:hover {
+  color: #0095ff;
+}
+</style>
