befd74c4e1bf54a8bd18b1eae0a577af6071f415
Ramzi Abdulahi <ramzi_abdullahi@outlook.com>
2018-07-26 13:12:54 -0400

    Added routes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,9 +3,12 @@ html within template, core code in scripts, and css in style */
 <template>
 <div class="pure-menu pure-menu-horizontal">
 <div class="container">
-<router-link :to="{ path: '/' }" class="pure-menu-heading pure-menu-link">Home</router-link</a>
+<router-link :to="{ path: '/' }" class="pure-menu-heading pure-menu-link"><img src="../assets/logo.jpg"/></router-link>
 <ul class="pure-menu-list">
+<li class="pure-menu-item"><router-link :to="{ path: '/best' }" class="pure-menu-link">Best</router-link></li>
 <li class="pure-menu-item"><router-link :to="{ path: '/new' }" class="pure-menu-link">New</router-link></li>
+<li class="pure-menu-item"><router-link :to="{ path: '/job' }" class="pure-menu-link">Jobs</router-link></li>
+<li class="pure-menu-item"><router-link :to="{ path: '/ask' }" class="pure-menu-link">Ask</router-link></li>
 </ul>
 </div>
 </div>
@@ -19,9 +22,13 @@ export default {
 
 <style scoped>
 .pure-menu{
-  background-color: #E17842;
+  background-color: #FF6600;
 }
 .pure-menu a{
   color: white;
 }
+  img {
+    width: 30px;
+    height: 30px;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 import Homepage from '@/components/Homepage'
 import Single from '@/components/Single'
 import New from '@/components/New'
+import Job from '@/components/Job'
+import Best from '@/components/Best'
+import Ask from '@/components/Ask'
 
 Vue.use(Router)
 
@@ -22,6 +25,21 @@ export default new Router({
       path: '/new',
       name: 'New',
       component: New
+    },
+    {
+      path: '/job',
+      name: 'Job',
+      component: Job
+    },
+    {
+      path: '/best',
+      name: 'Best',
+      component: Best
+    },
+    {
+      path: '/ask',
+      name: 'Ask',
+      component: Ask
     }
   ]
 })
